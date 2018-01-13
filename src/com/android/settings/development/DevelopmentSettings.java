8a2cc3fae8c77c22e475c4177aab02094ffb32b0
Bruno Martins <bgcngm@gmail.com>
2018-01-13 10:36:04 +0000

    DevelopmentSettings: Also hide OEM unlock from search results
    Change-Id: I4d5b90b119440118399511a7168050bf180695ec

--- src/com/android/settings/development/DevelopmentSettings.java
+++ src/com/android/settings/development/DevelopmentSettings.java
@@ -473,8 +473,7 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         mKeepScreenOn = (RestrictedSwitchPreference) findAndInitSwitchPref(KEEP_SCREEN_ON);
         mBtHciSnoopLog = findAndInitSwitchPref(BT_HCI_SNOOP_LOG);
         mEnableOemUnlock = (RestrictedSwitchPreference) findAndInitSwitchPref(ENABLE_OEM_UNLOCK);
-        if (!showEnableOemUnlockPreference(getActivity()) ||
-                !SystemProperties.getBoolean("ro.oem_unlock_supported", false)) {
+        if (!showEnableOemUnlockPreference(getActivity())) {
             removePreference(mEnableOemUnlock);
             mEnableOemUnlock = null;
         }
